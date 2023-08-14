77b214e98f1b93e8ed40bb6ab2b120b5029c3bc6
Mark <ajineer@outlook.com>
2023-08-14 12:11:10 -0600

    finished lab

--- server/seed.py
+++ server/seed.py
@@ -9,42 +9,43 @@ from models import db, Zookeeper, Animal, Enclosure
 
 fake = Faker()
 
-with app.app_context():
+if __name__ == "__main__":
+    with app.app_context():
 
-    Animal.query.delete()
-    Zookeeper.query.delete()
-    Enclosure.query.delete()
+        Animal.query.delete()
+        Zookeeper.query.delete()
+        Enclosure.query.delete()
 
-    zookeepers = []
-    for n in range(25):
-        zk = Zookeeper(name=fake.name(), birthday=fake.date_between(
-            start_date='-70y', end_date='-18y'))
-        zookeepers.append(zk)
+        zookeepers = []
+        for n in range(25):
+            zk = Zookeeper(name=fake.name(), birthday=fake.date_between(
+                start_date='-70y', end_date='-18y'))
+            zookeepers.append(zk)
 
-    db.session.add_all(zookeepers)
+        db.session.add_all(zookeepers)
 
-    enclosures = []
-    environments = ['Desert', 'Pond', 'Ocean', 'Field', 'Trees', 'Cave', 'Cage']
+        enclosures = []
+        environments = ['Desert', 'Pond', 'Ocean', 'Field', 'Trees', 'Cave', 'Cage']
 
-    for n in range(25):
-        e = Enclosure(environment=rc(environments), open_to_visitors=rc([True, False]))
-        enclosures.append(e)
+        for n in range(25):
+            e = Enclosure(environment=rc(environments), open_to_visitors=rc([True, False]))
+            enclosures.append(e)
 
-    db.session.add_all(enclosures)
+        db.session.add_all(enclosures)
 
-    animals = []
-    species = ['Lion', 'Tiger', 'Bear', 'Hippo', 'Rhino', 'Elephant', 'Ostrich',
-        'Snake', 'Monkey']
+        animals = []
+        species = ['Lion', 'Tiger', 'Bear', 'Hippo', 'Rhino', 'Elephant', 'Ostrich',
+            'Snake', 'Monkey']
 
-    for n in range(200):
-        name = fake.first_name()
-        while name in [a.name for a in animals]:
-            name=fake.first_name()
-        a = Animal(name=name, species=rc(species))
-        a.zookeeper = rc(zookeepers)
-        a.enclosure = rc(enclosures)
-        animals.append(a)
+        for n in range(200):
+            name = fake.first_name()
+            while name in [a.name for a in animals]:
+                name=fake.first_name()
+            a = Animal(name=name, species=rc(species))
+            a.zookeeper = rc(zookeepers)
+            a.enclosure = rc(enclosures)
+            animals.append(a)
 
-    db.session.add_all(animals)
-    db.session.commit()
+        db.session.add_all(animals)
+        db.session.commit()
 
